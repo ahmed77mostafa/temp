f5c80b8f8af83735dffbbc725210acf5330af4b3
Ahmed Mostafa <137928215+ahmed77mostafa@users.noreply.github.com>
2025-03-09 19:57:02 +0200

    More changes

--- main.c
+++ main.c
@@ -17,7 +17,7 @@ int main(void)
 	RCC->‎AHB1ENR‎ |= (0b01 << 1); // GPIOB
 	RCC->APB2ENR |= (0b01 << 8); // ADC1
 
-	ADC1->CR2 |= (0b01 << 0);   // ADON
+	ADC1->CR2 |= (0b01 << 0);   // Enable ADC
 	ADC1->CR1 |= (0b00 << 24);  // 12 bit
 
 	for(int i = 0; i < 1000; ++i);
@@ -25,10 +25,7 @@ int main(void)
 	ADC1->SQR3 = 0;      	   // Channel 0 -> PA0
 
 	//OUTPUTs
-	GPIOA->MODER |= (1 << (2 * GREENLED_PIN));
-	GPIOA->MODER |= (1 << (2 * YELLOWLED_PIN));
-	GPIOA->MODER |= (1 << (2 * REDLED_PIN));
-	GPIOB->MODER |= (1 << (2 * FAN_PIN));
+	GPIOA->MODER |= (1 << (2 * GREENLED_PIN) | 1 << (2 * YELLOWLED_PIN) | 1 << (2 * REDLED_PIN) | 1 << (2 * FAN_PIN) | 1 << (2 * BUZZER_PIN));
 
 	//INPUTs
 	GPIOA->MODER &= ~(0b11 << (2 * BUTTON_PIN));
@@ -46,6 +43,8 @@ int main(void)
 		int ADC_read = ADC1->DR;
 		float volt = (ADC_read * VREF)/ 4095; // read * 3.3 / 2 ^ resolution
 		float temp = volt * 100; // 10mv -> 1c
+
+	        GPIOA->ODR &= ~((1 << GREENLED_PIN) | (1 << YELLOWLED_PIN) | (1 << REDLED_PIN) | (1 << FAN_PIN) | (1 << BUZZER_PIN));
 
 		if(temp < 25)
 		{
